9bc84b2080c842f098d8dba737b56118a7513b09
Sun Ah Lee <sunahlee@Sun-Ahs-MacBook-Air-19.local>
2022-10-22 16:01:52 -0700

    Git repo initialization

---
+++ src/main/java/com/root/DoctorsIntegration/DoctorsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.root.DoctorsIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/root/DoctorsIntegration/DoctorsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root.DoctorsIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
